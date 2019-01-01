6c64e7f44a281b801cabfdc97782b158f7fe9f4e
DeDe <apple@DeDes-MacBook.local>
2018-12-31 21:22:12 -0800

    quick and dirty refactor

--- scripts/main.js
+++ scripts/main.js
@@ -17,6 +17,71 @@ class book
         this.pages = pages,
         this.read = read
     };
+    
+    addBookToLibrary() {
+        let b_title = title.value;
+        let b_author = author.value;
+        let b_pages = pages.value;
+        let b_read = read.checked;
+
+        if(!b_title || !b_author || !b_pages)
+        {
+            changeText('header','Missing or invalid data entered.');
+            return;
+        }
+        changeText('header','Add Book');
+        let newBook = new book(b_title, b_author, b_pages, b_read);
+        addBookToStorage(newBook);
+        displayLibrary(newBook);
+   }
+
+   updateStorage() {
+        localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+    }
+
+    loadStorage() {
+        if (!localStorage.myLibrary)
+            localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+        else {
+            JSON.parse(localStorage.getItem('myLibrary')).forEach(savedbook => {
+                savedbook.__proto__ = Object.create(book.prototype);
+                myLibrary.push(savedbook);
+            });
+        }
+        myLibrary.forEach(book => displayLibrary(book));
+    }
+
+    addBookToStorage(book) {
+        myLibrary.push(book);
+        updateStorage();
+    }
+
+    displayLibrary(newBook) {
+        table.innerHTML += `
+        <tr data-book="${myLibrary.indexOf(newBook)}">
+        <td class="libList">${newBook.title}</td>
+        <td class="libList">${newBook.author}</td>
+        <td class="libList">${newBook.pages}</td>
+        <td class="libList">${newBook.read ? 'Yes' : 'No'}</td>
+        </tr>`;
+    }
+
+    changeText(iD,newText) {
+        if (!iD || !newText)
+            return;
+        document.getElementById(iD).innerHTML = newText;
+    }
+
+    resetinputs() {
+        let inputElements = document.querySelectorAll('.inputId');
+        for(let i = 0; i < inputElements.length; i++)
+        {
+            if(inputElements[i].checked)
+                inputElements[i].checked = false;
+            inputElements[i].value = '';
+        }
+    }
+
 }
 
 addBook.addEventListener('click', e =>  {
@@ -30,68 +95,4 @@ newList.addEventListener('click', e => {
     localStorage.clear();
     location.reload();
 });
-
-function addBookToLibrary() {
-    let b_title = title.value;
-    let b_author = author.value;
-    let b_pages = pages.value;
-    let b_read = read.checked;
-
-    if(!b_title || !b_author || !b_pages)
-    {
-        changeText('header','Missing or invalid data entered.');
-        return;
-    }
-    changeText('header','Add Book');
-    let newBook = new book(b_title, b_author, b_pages, b_read);
-    addBookToStorage(newBook);
-   displayLibrary(newBook);
-}
-
-function updateStorage() {
-    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
-}
-
-function loadStorage() {
-    if (!localStorage.myLibrary)
-        localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
-    else {
-        JSON.parse(localStorage.getItem('myLibrary')).forEach(savedbook => {
-            savedbook.__proto__ = Object.create(book.prototype);
-            myLibrary.push(savedbook);
-        });
-    }
-    myLibrary.forEach(book => displayLibrary(book));
-}
-function addBookToStorage(book) {
-    myLibrary.push(book);
-    updateStorage();
-}
-
-function displayLibrary(newBook) {
-    table.innerHTML += `
-    <tr data-book="${myLibrary.indexOf(newBook)}">
-    <td class="libList">${newBook.title}</td>
-    <td class="libList">${newBook.author}</td>
-    <td class="libList">${newBook.pages}</td>
-    <td class="libList">${newBook.read ? 'Yes' : 'No'}</td>
-    </tr>`;
-}
-
-function changeText(iD,newText) {
-    if (!iD || !newText)
-        return;
-    document.getElementById(iD).innerHTML = newText;
-}
-
-function resetinputs()
-{
-    let inputElements = document.querySelectorAll('.inputId');
-    for(let i = 0; i < inputElements.length; i++)
-    {
-        if(inputElements[i].checked)
-                inputElements[i].checked = false;
-        inputElements[i].value = '';
-    }
-}
 loadStorage();

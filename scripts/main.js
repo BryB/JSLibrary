cca869a337234d9f5724bfbac830fe9498c2e059
DeDe <apple@DeDes-MacBook.local>
2019-01-01 15:03:54 -0800

    whoops

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,14 @@
 "use strict"
 
+let myLibrary = [];
+let table = document.getElementById('Library')
+let title = document.getElementById('title');
+let author = document.getElementById('author');
+let pages = document.getElementById('pages');
+let read = document.getElementById('read');
+let addBook = document.getElementById('submitB');
+let newList = document.getElementById('newList');
+
 class book
 {
     constructor (title, author, pages, read) {
@@ -8,81 +17,6 @@ class book
         this.pages = pages,
         this.read = read
     };
-    
-    let myLibrary = [];
-    let table = document.getElementById('Library')
-    let title = document.getElementById('title');
-    let author = document.getElementById('author');
-    let pages = document.getElementById('pages');
-    let read = document.getElementById('read');
-    let addBook = document.getElementById('submitB');
-    let newList = document.getElementById('newList');
-
-
-    function addBookToLibrary() {
-        let b_title = title.value;
-        let b_author = author.value;
-        let b_pages = pages.value;
-        let b_read = read.checked;
-
-        if(!b_title || !b_author || !b_pages)
-        {
-            changeText('header','Missing or invalid data entered.');
-            return;
-        }
-        changeText('header','Add Book');
-        let newBook = new book(b_title, b_author, b_pages, b_read);
-        addBookToStorage(newBook);
-        displayLibrary(newBook);
-   }
-
-    function updateStorage() {
-        localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
-    }
-
-    function loadStorage() {
-        if (!localStorage.myLibrary)
-            localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
-        else {
-            JSON.parse(localStorage.getItem('myLibrary')).forEach(savedbook => {
-                savedbook.__proto__ = Object.create(book.prototype);
-                myLibrary.push(savedbook);
-            });
-        }
-        myLibrary.forEach(book => displayLibrary(book));
-    }
-
-    function addBookToStorage(book) {
-        myLibrary.push(book);
-        updateStorage();
-    }
-
-    function displayLibrary(newBook) {
-        table.innerHTML += `
-        <tr data-book="${myLibrary.indexOf(newBook)}">
-        <td class="libList">${newBook.title}</td>
-        <td class="libList">${newBook.author}</td>
-        <td class="libList">${newBook.pages}</td>
-        <td class="libList">${newBook.read ? 'Yes' : 'No'}</td>
-        </tr>`;
-    }
-
-    function changeText(iD,newText) {
-        if (!iD || !newText)
-            return;
-        document.getElementById(iD).innerHTML = newText;
-    }
-
-    function resetinputs() {
-        let inputElements = document.querySelectorAll('.inputId');
-        for(let i = 0; i < inputElements.length; i++)
-        {
-            if(inputElements[i].checked)
-                inputElements[i].checked = false;
-            inputElements[i].value = '';
-        }
-    }
-
 }
 
 addBook.addEventListener('click', e =>  {
@@ -96,4 +30,68 @@ newList.addEventListener('click', e => {
     localStorage.clear();
     location.reload();
 });
+
+function addBookToLibrary() {
+    let b_title = title.value;
+    let b_author = author.value;
+    let b_pages = pages.value;
+    let b_read = read.checked;
+
+    if(!b_title || !b_author || !b_pages)
+    {
+        changeText('header','Missing or invalid data entered.');
+        return;
+    }
+    changeText('header','Add Book');
+    let newBook = new book(b_title, b_author, b_pages, b_read);
+    addBookToStorage(newBook);
+   displayLibrary(newBook);
+}
+
+function updateStorage() {
+    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+}
+
+function loadStorage() {
+    if (!localStorage.myLibrary)
+        localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+    else {
+        JSON.parse(localStorage.getItem('myLibrary')).forEach(savedbook => {
+            savedbook.__proto__ = Object.create(book.prototype);
+            myLibrary.push(savedbook);
+        });
+    }
+    myLibrary.forEach(book => displayLibrary(book));
+}
+function addBookToStorage(book) {
+    myLibrary.push(book);
+    updateStorage();
+}
+
+function displayLibrary(newBook) {
+    table.innerHTML += `
+    <tr data-book="${myLibrary.indexOf(newBook)}">
+    <td class="libList">${newBook.title}</td>
+    <td class="libList">${newBook.author}</td>
+    <td class="libList">${newBook.pages}</td>
+    <td class="libList">${newBook.read ? 'Yes' : 'No'}</td>
+    </tr>`;
+}
+
+function changeText(iD,newText) {
+    if (!iD || !newText)
+        return;
+    document.getElementById(iD).innerHTML = newText;
+}
+
+function resetinputs()
+{
+    let inputElements = document.querySelectorAll('.inputId');
+    for(let i = 0; i < inputElements.length; i++)
+    {
+        if(inputElements[i].checked)
+                inputElements[i].checked = false;
+        inputElements[i].value = '';
+    }
+}
 loadStorage();
